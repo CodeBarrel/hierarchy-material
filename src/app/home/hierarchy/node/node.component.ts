6a2547a1bcbf34a1bf6ba58940183278ffabc78b
Muhammad Iqbal Hussain <iqbal.hussain@gaditek.com>
2018-02-28 13:49:48 +0500

    added options in node component

--- src/app/home/hierarchy/node/node.component.ts
+++ src/app/home/hierarchy/node/node.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { trigger,state,style,transition,animate } from '@angular/animations';
+import { trigger,state,style,transition,animate,group } from '@angular/animations';
 
 class Node {
   public header: string;
@@ -14,7 +14,7 @@ class Node {
   templateUrl: './node.component.html',
   styleUrls: ['./node.component.scss'],
   animations: [
-    trigger('collapse',[
+    /*trigger('collapse',[
       state('in', style({
         backgroundColor: 'transparent',
         opacity: '1',
@@ -23,6 +23,38 @@ class Node {
         opacity: '0',
       })),
       transition('in => out', animate('300ms ease-in')),
+    ]),*/
+    trigger('slideInOut', [
+      state('in', style({
+        'max-height': '500px', 'opacity': '1', 'visibility': 'visible'
+      })),
+      state('out', style({
+        'max-height': '0px', 'opacity': '0', 'visibility': 'hidden'
+      })),
+      transition('in => out', [group([
+          animate('400ms ease-in-out', style({
+            'opacity': '0'
+          })),
+          animate('600ms ease-in-out', style({
+            'max-height': '0px'
+          })),
+          animate('700ms ease-in-out', style({
+            'visibility': 'hidden'
+          }))
+        ]
+      )]),
+      transition('out => in', [group([
+          animate('1ms ease-in-out', style({
+            'visibility': 'visible'
+          })),
+          animate('600ms ease-in-out', style({
+            'max-height': '500px'
+          })),
+          animate('800ms ease-in-out', style({
+            'opacity': '1'
+          }))
+        ]
+      )])
     ]),
   ]
 })
@@ -31,6 +63,10 @@ export class NodeComponent implements OnInit {
   @Input() node: Node;
   wasClicked = false;
   state: string = 'in';
+  option: boolean = false;
+
+  animationState = 'in';
+
   constructor() { }
 
   ngOnInit() {
@@ -46,6 +82,21 @@ export class NodeComponent implements OnInit {
     //console.log(parent);
     this.state = (this.state === 'in' ? 'out' : 'in');
     this.wasClicked= !this.wasClicked;
+  }
+  activeOptions(option: any){
+    (option == true) ? this.option = false : this.option = true
+  }
+  editNode(){
+    alert('1');
+  }
+
+  toggleShowDiv(divName: string) {
+    /*if (divName === 'divA') {
+      console.log(this.animationState);
+
+      console.log(this.animationState);
+    }*/
+    this.animationState = this.animationState === 'out' ? 'in' : 'out';
   }
 
 }
